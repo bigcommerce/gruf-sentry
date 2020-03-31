949d4dd440818453971c5fe574f1d88f213ee8a0
Shaun McCormick <splittingred@gmail.com>
2020-03-31 16:07:41 -0500

    README updates

--- spec/simplecov_helper.rb
+++ spec/simplecov_helper.rb
@@ -24,7 +24,7 @@ if File.directory?('./coverage/')
 end
 
 SimpleCov.command_name 'Unit Tests'
-SimpleCov.minimum_coverage 90
+SimpleCov.minimum_coverage 80
 SimpleCov.start do
   add_filter '/bin/'
   add_filter '/coverage/'
